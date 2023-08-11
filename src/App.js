637ed8c05849b203d6156b85d50419135f8b5760
Benjamin Miller <miller@Benjamins-MacBook-Air.local>
2023-08-11 22:19:43 +0100

    Board and sqaure setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-//import './App.css';
+import './App.css';
 import { useState } from 'react';
 
-export default function Square() {
+function Square({value}) {
 
-  return <button className="square">X</button>
+  return (
+    <button
+      className="square">
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+
+  const [sqaures, setSquares] = useState(Array(9).fill(null));
+
+  return (
+    <>
+      <div className='board-row'>
+        <Square value = {sqaures[0]}/>
+        <Square value = {sqaures[1]}/>
+        <Square value = {sqaures[2]}/>
+      </div>
+      <div className='board-row'>
+        <Square value = {sqaures[3]}/>
+        <Square value = {sqaures[4]}/>
+        <Square value = {sqaures[5]}/>
+      </div>
+      <div className='board-row'>
+        <Square value = {sqaures[6]}/>
+        <Square value = {sqaures[7]}/>
+        <Square value = {sqaures[8]}/>
+      </div>
+    </>
+  );
 };
