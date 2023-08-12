3720bb3f362ecf06058a1ab77861a684368864d8
Benjamin Miller <miller@Benjamins-MacBook-Air.local>
2023-08-12 14:33:10 +0100

    X placed in correct square when clicked

--- src/App.js
+++ src/App.js
@@ -16,28 +16,28 @@ export default function Board() {
 
   const [sqaures, setSquares] = useState(Array(9).fill(null));
 
-  const handleClick = () => {
+  const handleClick = (i) => {
     const nextSquares = sqaures.slice();
-    nextSquares[0] = 'X';
+    nextSquares[i] = 'X';
     setSquares(nextSquares);
   };
 
   return (
     <>
       <div className='board-row'>
-        <Square value = {sqaures[0]} onSquareClick={handleClick}/>
-        <Square value = {sqaures[1]} onSquareClick={handleClick}/>
-        <Square value = {sqaures[2]} onSquareClick={handleClick}/>
+        <Square value = {sqaures[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value = {sqaures[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value = {sqaures[2]} onSquareClick={() => handleClick(2)}/>
       </div>
       <div className='board-row'>
-        <Square value = {sqaures[3]} onSquareClick={handleClick}/>
-        <Square value = {sqaures[4]} onSquareClick={handleClick}/>
-        <Square value = {sqaures[5]} onSquareClick={handleClick}/>
+        <Square value = {sqaures[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value = {sqaures[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value = {sqaures[5]} onSquareClick={() => handleClick(5)}/>
       </div>
       <div className='board-row'>
-        <Square value = {sqaures[6]} onSquareClick={handleClick}/>
-        <Square value = {sqaures[7]} onSquareClick={handleClick}/>
-        <Square value = {sqaures[8]} onSquareClick={handleClick}/>
+        <Square value = {sqaures[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value = {sqaures[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value = {sqaures[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </>
   );
